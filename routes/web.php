7d915d6b2d9446f30af4a05b0ccfc3f3a3660e5c
Alberto Hermosillo <albertoherms17@gmail.com>
2024-03-10 17:09:08 -0600

    fix: initial route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NotificationController;
 
+Route::get('/', [NotificationController::class, 'showForm'])->name('home');
 Route::get('/notifications', [NotificationController::class, 'showForm'])->name('notifications.index');
 Route::post('/notification/send', [NotificationController::class, 'sendNotification'])->name('notification.send');
 
